c2eb8dc1fa4328736b9f0285df88c253564e3cfa
d-hooper <dallinhoop3@gmail.com>
2025-03-03 14:15:27 -0700

    receiving and logging response data from trivia api

--- app/controllers/questionsController.js
+++ app/controllers/questionsController.js
@@ -1,9 +1,19 @@
+import { questionsService } from "../services/questionsService.js";
 
 
 export class QuestionsController {
 
-constructor() {
-  
-}
+  constructor() {
+    console.log('❓ controller reporting in?');
+    this.getQuestions()
+  }
+
+  async getQuestions() {
+    try {
+      await questionsService.getQuestions()
+    } catch (error) {
+      console.error('Could not retrieve any questions')
+    }
+  }
 
 } 
--- app/services/questionsService.js
+++ app/services/questionsService.js
@@ -1,6 +1,15 @@
-
+// @ts-ignore
+const questionsApi = axios.create({
+  baseURL: 'https://opentdb.com/api.php?amount=10&category=15&type=multiple',
+  timeout: 2000
+})
 
 class QuestionsService {
+  async getQuestions() {
+    const response = await questionsApi.get()
+    console.log('❓❓❓ received');
+    console.log(response);
+  }
 
 
 }
